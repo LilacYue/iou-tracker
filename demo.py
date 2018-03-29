325c39238fbd0024b2c6d2e96e231f32d608c48e
jeffshih <jeffhfs1224@gmail.com>
2018-03-29 11:25:46 +0800

    change demo

--- demo.py
+++ demo.py
@@ -1,47 +1,231 @@
-#!/usr/bin/env python
+import numpy as np
+import cv2
+from ainvr import ainvr
+from iou_tracker.iou_tracker import Tracker
+from pc_database import Database
+import time
+import datetime
 
-# ---------------------------------------------------------
-# IOU Tracker
-# Copyright (c) 2017 TU Berlin, Communication Systems Group
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Erik Bochinski
-# ---------------------------------------------------------
-
-from time import time
-import argparse
-
-from iou_tracker import track_iou
-from util import load_mot, save_to_csv
-
-
-def main(args):
-    detections = load_mot(args.detection_path)
-
-    start = time()
-    tracks = track_iou(detections, args.sigma_l, args.sigma_h, args.sigma_iou, args.t_min)
-    end = time()
-
-    num_frames = len(detections)
-    print("finished at " + str(int(num_frames / (end - start))) + " fps!")
-
-    save_to_csv(args.output_path, tracks)
+db = Database()
+camera_id = 1
+font = cv2.FONT_HERSHEY_DUPLEX
+font_size = 0.8
+skip_ms = 66
+iou_thresh = 0.1
+nms_thresh = 0.2
+height = 1080 
+width = 1920
+resize_ratio = 3
+target_labels = ['person']
+direction = 0; #0,1 change in, out direction
 
 
-if __name__ == '__main__':
+#target_video="rtsp://admin:admin@172.16.26.241:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif"
 
-    parser = argparse.ArgumentParser(description="IOU Tracker demo script")
-    parser.add_argument('-d', '--detection_path', type=str, required=True,
-                        help="full path to CSV file containing the detections")
-    parser.add_argument('-o', '--output_path', type=str, required=True,
-                        help="output path to store the tracking results (MOT challenge devkit compatible format)")
-    parser.add_argument('-sl', '--sigma_l', type=float, default=0,
-                        help="low detection threshold")
-    parser.add_argument('-sh', '--sigma_h', type=float, default=0.5,
-                        help="high detection threshold")
-    parser.add_argument('-si', '--sigma_iou', type=float, default=0.5,
-                        help="intersection-over-union threshold")
-    parser.add_argument('-tm', '--t_min', type=float, default=2,
-                        help="minimum track length")
+#target_video="rtsp://root:ironyun182@172.16.12.214:554/axis-media/media.amp"
 
-    args = parser.parse_args()
-    main(args)
+#target_video="rtsp://172.16.12.213:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif"
+
+target_video="rtsp://172.16.26.244:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif"
+
+#target_video="rtsp://admin:admin@172.16.22.195:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif"
+
+line = [[750/resize_ratio,300/resize_ratio],[500/resize_ratio,1100/resize_ratio]]
+
+line_width = 20
+
+def ccw(A,B,C):
+    return (C[1]-A[1])*(B[0]-A[0]) > (B[1]-A[1])*(C[0]-A[0])
+
+def line_intersection(line1,line2):
+    A=line1[0]
+    B=line1[1]
+    C=line2[0]
+    D=line2[1]
+    #print A,B,C,D
+    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+
+def checkOrientation(line1,line2):
+    A=line1[0]
+    B=line1[1]
+    C=line2[0]
+    D=line2[1]
+    return ccw(A,D,C)
+
+
+def render_detections(im, detections):
+    for detection in detections:
+    	xmin = int(detection["xmin"])
+    	ymin = int(detection["ymin"])
+    	xmax = int(detection["xmax"])
+    	ymax = int(detection["ymax"])
+    	label = detection["class"]
+        width = xmax - xmin
+        height = ymax - ymin
+	
+        highlight_W = xmin + len(label) * 14
+        highlight_H = ymin + height
+        cv2.rectangle(im, (xmin,ymin),(xmin+width,ymin+height),(0,255,0),2)
+        cv2.rectangle(im, (xmin,ymin+height+14),(highlight_W, highlight_H),(0,255,0),-1)
+        cv2.putText(im, label, (xmin, highlight_H+14), font, font_size, (0,0,0),1)       
+    return im
+
+
+
+def render_tracks(im, tracks):
+    for id_, track in tracks.items():
+        xmin, ymin, xmax, ymax = track['bbox']
+        xmin = int(xmin)
+        ymin = int(ymin)
+        width = int(xmax - xmin)
+        height = int(ymax - ymin)
+        label = str(id_)
+        highlight_W = xmin + len(label) * 14
+        highlight_H = ymin + height
+        cv2.rectangle(im, (xmin,ymin),(xmin+width,ymin+height),(0,255,0),2)
+        cv2.rectangle(im, (xmin,ymin+height+14),(highlight_W, highlight_H),(0,255,0),-1)
+        cv2.putText(im, label, (xmin, highlight_H+14), font, font_size, (0,0,0),1)       
+    return im
+
+
+def render_tracks(im, tracks,buf,cntIn,cntOut):
+    prev = buf[0]
+    blue = buf[1]
+    red  = buf[2]
+    last = cntIn-cntOut
+    dIcnt = cntIn
+    dOcnt = cntOut
+    for id_, track in tracks.items():
+        xmin, ymin, xmax, ymax = track['bbox']
+        xmin = int(xmin)
+        ymin = int(ymin)
+        xmax = int(xmax)
+        ymax = int(ymax)
+        #width = int(xmax - xmin)
+        height = int(ymax - ymin)
+        
+        rectagleCenterPont = (int((xmin + xmax) /2), int((ymin + ymax) /2))
+        #print rectagleCenterPont
+        cv2.circle(im, rectagleCenterPont, 1, (0, 0, 255), 5)
+        
+        if(prev.has_key(id_)):
+            #line = [[(xmin + xmax) /2, (ymin + ymax) /2],[prev[id_][0]],prev[id_][1]]
+            lineToCheck = [[rectagleCenterPont[0],rectagleCenterPont[1]],prev[id_]]
+            #if ((rectagleCenterPont[1]**2-prev[id_][1]**2)>40000):
+            #    continue
+            #print lineToCheck,id_
+            if(line_intersection(redLine,lineToCheck)):
+                #print lineToCheck
+                if blue.has_key(id_):
+                    cntOut += 1
+                    blue.pop(id_)
+                elif (blue.has_key(id_)==False):
+                    red[id_] = [rectagleCenterPont[0],rectagleCenterPont[1]]
+            if(line_intersection(blueLine,lineToCheck)):
+                #print lineToCheck
+                if red.has_key(id_): 
+                    cntIn += 1
+                    red.pop(id_)
+                elif (red.has_key(id_)==False): 
+                    blue[id_] = [rectagleCenterPont[0],rectagleCenterPont[1]]
+                    
+        prev[id_] = [rectagleCenterPont[0],rectagleCenterPont[1]]
+        
+        label = str(id_)
+        highlight_W = xmin + len(label) * 14
+        highlight_H = ymax
+        cv2.rectangle(im, (xmin,ymin),(xmax,ymax),(0,255,0),2)
+        cv2.rectangle(im, (xmin,ymax+14),(highlight_W, highlight_H),(0,255,0),-1)
+        cv2.putText(im, label, (xmin, highlight_H+14), font, font_size, (0,0,0),1)
+    if (direction == 1) :
+        cv2.putText(frame, "in : {}".format(str(cntIn)), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+        cv2.putText(frame, "out: {}".format(str(cntOut)), (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+    else :
+        cv2.putText(frame, "in : {}".format(str(cntOut)), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+        cv2.putText(frame, "out: {}".format(str(cntIn)), (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+   # dTcnt = cntIn-cntOut-last
+    dIcnt = cntIn-dIcnt
+    dOcnt = cntOut-dOcnt
+    return im,cntIn,cntOut,dIcnt,dOcnt
+  
+
+
+def parse_detections(detections, target_labels):
+    parsed_detections = []
+    for detection in detections:
+	    if detection['class'] in target_labels:
+	        score = detection["score"]
+	        xmin = detection['xmin']
+	        xmax = detection['xmax']
+	        ymin = detection['ymin']
+	        ymax = detection['ymax'] 
+	        bbox = (xmin, ymin, xmax,  ymax)
+                #if ((ymax-ymin)>30) :
+                #    parsed_detections.append({"bbox":bbox, "score":score})
+	        parsed_detections.append({"bbox":bbox, "score":score})	
+    return parsed_detections
+
+cap = cv2.VideoCapture(target_video)
+tracker = Tracker(sigma_iou = iou_thresh,t_max = 5)
+
+
+buf = [{},{},{}]  
+cntIn = 0
+cntOut = 0
+lw = line_width
+blueLine = [[line[0][0]-lw,line[0][1]-lw],[line[1][0]-lw,line[1][1]-lw]]
+redLine = [[line[0][0]+lw,line[0][1]+lw],[line[1][0]+lw,line[1][1]+lw]]
+
+last_ms = int(round(time.time() * 1000))
+
+print "width" + str(width)
+print "height" + str(height)
+
+while(True):
+    # Capture frame-by-frame
+    now_ms = int(round(time.time() * 1000))
+    if (now_ms - last_ms)<skip_ms:
+        cap.grab()
+        continue
+
+    #print (now_ms - last_ms)
+    last_ms = now_ms
+    ret, frame = cap.read()
+    if frame is None:
+	print("Empty frame received!")
+	print(cap.isOpened())
+	continue
+
+    frame = cv2.resize(frame,(width/resize_ratio,height/resize_ratio))
+    detections = ainvr.detect_img(frame,NMS_THRESH=nms_thresh)
+    parsed_detections = parse_detections(detections,target_labels)
+    tracks = tracker.track(parsed_detections)
+    
+    print(tracks)
+    
+    #dI/Ocnt is the change of people count by each frame
+    frame,cntIn,cntOut,dIcnt,dOcnt= render_tracks(frame, tracks,buf,cntIn,cntOut)
+
+    if (dIcnt>0 or dOcnt>0) :
+        timestamp=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        if (direction == 1) :
+            db.addCountEvent(camera_id, timestamp, dIcnt, dOcnt)
+        else :
+            db.addCountEvent(camera_id, timestamp, dOcnt, dIcnt)
+    cv2.line(frame, (blueLine[0][0],blueLine[0][1]), (blueLine[1][0],blueLine[1][1]), (250, 0, 0), 1) #blue line
+    cv2.line(frame, (redLine[0][0],redLine[0][1]), (redLine[1][0],redLine[1][1]), (0, 0, 255), 1)#red line
+    if (direction == 1) :
+        cv2.putText(frame, "in", ((blueLine[0][0]+blueLine[1][0])/2-10, (blueLine[0][1]+blueLine[1][1])/2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+        cv2.putText(frame, "out", ((redLine[0][0]+redLine[1][0])/2-10, (redLine[0][1]+redLine[1][1])/2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+    else :
+        cv2.putText(frame, "out", ((blueLine[0][0]+blueLine[1][0])/2-10, (blueLine[0][1]+blueLine[1][1])/2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+        cv2.putText(frame, "in", ((redLine[0][0]+redLine[1][0])/2-10, (redLine[0][1]+redLine[1][1])/2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
--- iou_tracker.py
+++ iou_tracker.py
@@ -6,10 +6,70 @@
 # ---------------------------------------------------------
 
 from time import time
-
 from util import load_mot, iou
 
+class Tracker():
+    def __init__(self, sigma_l=0, sigma_h=0.5, sigma_iou=0.2, t_max=5, logging = False):
+        self.sigma_l = sigma_l
+        self.sigma_h = sigma_h
+        self.sigma_iou = sigma_iou
+        self.t_max = t_max
+        self.frame = 0
+        self.id_count = 0
+        self.tracks_active = {}
 
+	self.logging = logging
+	self.log = {0:[]}
+    #Clear the old tracks
+    def clean_old_tracks(self):
+        target_frame = self.frame - self.t_max
+        if self.tracks_active.has_key(target_frame):
+            if self.logging:
+		self.log[self.frame]=["[Log]: Tracks Deleted:{}".format(self.tracks_active[target_frame].keys())]
+	    del(self.tracks_active[target_frame])
+	
+    #Retrieve tracks in an correct matching order
+    def retrieve_tracks(self):
+        tracks = []
+        selected_tracks = {}
+        frames = range(self.frame, self.frame - self.t_max, -1)
+        for frame in frames:
+            if frame in self.tracks_active:
+                for id_, track in self.tracks_active[frame].items():
+                    if id_ not in selected_tracks:
+                        #tracks += (id_,track)
+                        tracks += self.tracks_active[frame].items()
+			selected_tracks[id_]=track
+                #tracks += self.tracks_active[frame].items()
+        return tracks
+
+    def track(self, detections):
+        self.frame += 1
+        self.tracks_active[self.frame] = {}
+        #Clear the tracks in old frame
+        self.clean_old_tracks()
+
+        dets = [det for det in detections if det['score'] >= self.sigma_l]
+        
+        for id_, track in self.retrieve_tracks():
+            if len(dets) > 0:
+                # get det with highest iou
+                best_match = max(dets, key=lambda x: iou(track['bbox'], x['bbox']))
+                if iou(track['bbox'], best_match['bbox']) >= self.sigma_iou:
+                    self.tracks_active[self.frame][id_] = best_match                 
+                    # remove from best matching detection from detections
+                    del dets[dets.index(best_match)]
+
+        #Create new tracks
+        for det in dets:
+            self.id_count += 1
+            self.tracks_active[self.frame][self.id_count] = det
+
+        #Return the current tracks 
+        return self.tracks_active[self.frame]  
+
+
+  
 def track_iou(detections, sigma_l, sigma_h, sigma_iou, t_min):
     """
     Simple IOU based tracker.
